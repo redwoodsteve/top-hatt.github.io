d1ea7b4cbb8b8da283fd007a8514665883e808f3
Top-Hatt <146483282+Top-Hatt@users.noreply.github.com>
2023-10-16 21:20:44 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,17 +1,1 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/solve', methods=['POST'])
-def solve():
-    problem = request.form['problem']
-    # Now, go wild solving this problem using some magical code.
-    # Return the result to the user.
-    return render_template('result.html', result=result)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+<iframe src="chat.openai.com" width="600" height="400"></iframe>
