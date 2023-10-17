b70e5d8aa2ad450b38904de7bd61995afdb68d06
Top-Hatt <146483282+Top-Hatt@users.noreply.github.com>
2023-10-16 21:23:33 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,19 @@
-<iframe src="chat.openai.com" width="600" height="400"></iframe>
+from flask import Flask, render_template, request
+import openai
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    api_key = request.form['api_key']
+    openai.api_key = api_key
+    # Now, you have access to the AI! Do something magical here.
+    # Return the AI-generated result to the user.
+    return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
